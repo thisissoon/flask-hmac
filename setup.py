4a5c89ae007d80c7c5adb28be13aa724ae9c65a9
Jamon Camisso <jamon@jamon.ca>
2014-05-28 21:22:51 -0400

    Updated long_documentation with README.rst

--- setup.py
+++ setup.py
@@ -4,16 +4,18 @@ Flask-HMAC
 """
 from setuptools import setup
 
+with open('README.rst') as file:
+    long_description = file.read()
 
 setup(
     name='Flask-HMAC',
-    version='0.1',
-    url='http://github.com/jamonation/flask-hmac/',
+    version='0.1.1',
+    url='http://github.com/jamonation/flask-hmac',
     license='WTFPL',
     author='Jamon Camisso',
     author_email='jamonation+flask@gmail.com',
     description='Flask HMAC generator, checker, and route decorator',
-    long_description=__doc__,
+    long_description=long_description,
     py_modules=['flask_hmac'],
     zip_safe=False,
     include_package_data=True,
